dec0387e90906c526f95bc73e8fd76b46cf3bd7a
row248 <spamanet66@gmail.com>
2021-06-24 15:37:11 +0300

    add POSTGRES_HOST env variable

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatModule } from './chat/chat.module';
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     ChatModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.POSTGRES_HOST,
       port: 5432,
       username: 'postgres',
       password: 'postgres',
